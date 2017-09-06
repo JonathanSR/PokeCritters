62c9dc430c032aaf255e41e6ac0364456233a780
Jonathan Serrano <jonathan.serranor@gmail.com>
2017-09-06 08:54:52 -0600

    creates user logs in spec

--- spec/features/user/user_can_log_in_spec.rb
+++ spec/features/user/user_can_log_in_spec.rb
@@ -3,4 +3,19 @@ require 'rails_helper'
 RSpec.feature "User" do
   context "appropriate login" do
     scenario "user logs in and is directed to their dashboard" do
-      
+      user = create(:user)
+
+      visit login_path
+
+      fill_in "session[email]", with: user.email
+      fill_in "session[password]", with: user.password
+
+      click_button "Catch Em All"
+
+      expect(current_path).to eq(pokedex_path)
+      expect(page).to have_content("#{user.username}'s Pokedex")
+      expect(page).to_not have_content("Login")
+      expect(page).to have_link("Logout")
+    end
+  end
+end
