42d030c2fdf82285d3bd477e48bde8db854d586a
Jonathan Serrano <jonathan.serranor@gmail.com>
2017-09-07 15:40:48 -0600

    new user can be created
    Adds new and create method to user model.
    Adds new html erb for user.
    Configures route file.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,7 +16,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session.clear
-    flash[:success] = "Logged Out"
+    flash[:danger] = "Logged Out"
     redirect_to root_path
   end
 
